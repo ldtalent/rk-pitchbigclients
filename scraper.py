a42e08d3bf27a6694cd0074a05505cb15d4a5987
Rashika Karki <41114269+RashikaKarki@users.noreply.github.com>
2020-07-10 07:57:31 +0545

    scrape engineer list

--- scraper.py
+++ scraper.py
@@ -6,14 +6,76 @@ import csv
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import pandas as pd
+from time import sleep
+
+#scrape skills from LD
+def getskills():
+    driver = webdriver.Chrome()
+    mainurl = 'https://www.learningdollars.com/client/select_engineers/'
+    driver.get(mainurl)   
+    categorylist=driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/form/div[1]/div[1]/div/select/option')
+    category = []
+    skills = []
+    for item in categorylist[1:]:
+        category.append(item.text)
+        skillslist = []
+        item.click()
+        skillslist=driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/form/div[1]/div[2]/div/select/option')
+        skills.append([i.text for i in skillslist])        
+    driver.close()
+    categoryskill = {}
+    count = 0
+    for i in category:
+        if count == 0:
+            categoryskill[i] = sorted(skills[count])
+        else:
+            skillcurrent = set(skills[count])
+            skillprevious = set(skills[count - 1])
+            skillfinal = skillcurrent-skillprevious
+            categoryskill[i] = sorted(list(skillfinal))
+        count = count + 1
+    return categoryskill
+
+#Function to get link
+def getlink(skilllist):
+    driver = webdriver.Chrome()
+    mainurl = 'https://www.learningdollars.com/client/select_engineers/'
+    driver.get(mainurl)   
+    categorylist=driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/form/div[1]/div[1]/div/select/option')
+    #Different bottons
+    selectbotton = driver.find_element_by_xpath('//*[@id="move-right"]')
+    findengs = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/form/button[1]')
+    #Selecting category
+    categorylist[0].click()    
+    skills=driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div[2]/form/div[1]/div[2]/div/select/option')
+    for skill in skills:
+        if str(skill.text) in skilllist:
+            skill.click()
+            selectbotton.click()
+    findengs.click()
+    getlink = driver.find_element_by_xpath('//*[@id="shareable_link_btn"]')
+    getlink.click()
+    sleep(5)
+    link = driver.find_element_by_xpath('//*[@id="shareable_link_text"]').text
+    sleep(5)
+    driver.close()
+    return link
 
 #main function with all the functionality
 def scrape(filename,driver,jobs,mode):
+    #Getting skills to compare    
+    ldskills = getskills()
+    ldcategory = [*ldskills]
+    allskills = []
+    for category in ldcategory:
+        allskills.extend(ldskills[category])
+    #Setting writing modes
     if mode == 1:
         filemode = 'a'
     else:
         filemode = 'w'
-
+    #Sorting data for append mode
     if filemode == 'a':
         df = pd.read_csv(filename, encoding = "ISO-8859-1")
         df['Datetime'] = pd.to_datetime(df.Datetime, infer_datetime_format = True)
@@ -22,7 +84,7 @@ def scrape(filename,driver,jobs,mode):
     with open(filename, filemode, newline='',  encoding="utf-8") as file:
         writer = csv.writer(file)
         if filemode == 'w':
-            writer.writerow(["Datetime", "Name", "Job_title","Skills","Description"])            
+            writer.writerow(["Datetime", "Name", "Job_title","Skills","Description","Engineers_link"])            
         #Extracting data    
         for i in jobs:
             skill = []
@@ -32,7 +94,13 @@ def scrape(filename,driver,jobs,mode):
             companytitle = companyjob.find("h2").text
             tags = job.find(class_ = "tags").find_all(class_=re.compile("^tag"))
             for tag in tags:
-                skill.append(tag.find('h3').text)
+                tagname = str(tag.find('h3').text).title()
+                #Condition to check if the skill exist in the job descriptionq
+                if tagname in allskills:
+                    skill.append(tagname)
+                else:
+                    continue
+
             time = job.find("time")['datetime']            
             time = datetime.datetime.strptime(''.join(time.rsplit(':', 1)), '%Y-%m-%dT%H:%M:%S%z')
             #Extract Description
@@ -55,15 +123,26 @@ def scrape(filename,driver,jobs,mode):
                 descriptionfinal = dict()
                 descriptionfinal['description'] = "Not Found"
             driver.execute_script("arguments[0].click();", clickBot)
+
+            #Extracting skill from description
+            for allskill in allskills:
+                if allskill.lower() in str(descriptionfinal['description']).lower():
+                    if (len(allskill)>3):
+                        skill.append(allskill)          
+
             if filemode == 'w':
-                writer.writerow([time,companyname, companytitle, skill, descriptionfinal['description']])
-                print("Added new job: ", companytitle )
+                if len(skill)>3:
+                    link = getlink(skill)
+                    writer.writerow([time,companyname, companytitle, skill, descriptionfinal['description'], link])
+                    print("Added new job: ", companytitle )
             else:
                 if (time <= df['Datetime'][0]):
                     break
                 else:
-                    writer.writerow([time,companyname, companytitle, skill, descriptionfinal['description']])
-                    print("Added new job: ", companytitle )
+                    if len(skill)>=1:
+                        link = getlink(skill)
+                        writer.writerow([time,companyname, companytitle, skill, descriptionfinal['description'], link])
+                        print("Added new job: ", companytitle )
     
 
 def scrapemode(file, mode):
